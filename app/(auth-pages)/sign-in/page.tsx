f5f9e6df19a0de1310b3ae6ac9b475595321a69a
Scott Ginebaugh <spginebaugh@gmail.com>
2025-01-12 01:32:20 -0500

    fixed searchparams

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import LoginForm from './login-form'
 
 interface PageProps {
-  searchParams?: { message?: string }
+  searchParams: { message?: string }
 }
 
 export default function Page({ searchParams }: PageProps) {
